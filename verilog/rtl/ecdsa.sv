2b44e5be50f889e6e88a5c644282696dd376aa78
Peter Drotos <peter.drotos@outlook.com>
2026-04-15 13:19:17 +0000

    update ECDSA to use BRAM

--- verilog/rtl/ecdsa.sv
+++ verilog/rtl/ecdsa.sv
@@ -176,9 +176,11 @@ module ecdsa
     localparam int POINT_ADD_END   = POINT_ADD_START + POINT_ADD_LEN - 1;
     localparam int FINALIZE_START  = POINT_ADD_END   + 1;
     localparam int FINALIZE_END    = FINALIZE_START  + FINALIZE_LEN  - 1;
+    localparam int BLANK_START     = FINALIZE_END    + 1;
 
-    // Array to collect the PC values where execution should automatically stop
-    localparam int PROGRAM_ENDS [3] = '{PREPARE_END, POINT_ADD_END, FINALIZE_END};
+    // Arrays to collect the PC values
+    localparam int PROGRAM_STARTS [4] = '{PREPARE_START, POINT_ADD_START, FINALIZE_START, BLANK_START};
+    localparam int PROGRAM_ENDS   [3] = '{PREPARE_END,   POINT_ADD_END,   FINALIZE_END};
 
     // -------------------------------------------------------------------------
     // FSM states
@@ -193,15 +195,54 @@ module ecdsa
     } state_e;
 
     // -------------------------------------------------------------------------
+    // Register file RAM
+    // -------------------------------------------------------------------------
+
+
+    // Register file, Two-port (2 read, 1 write) RAM
+    (* ram_style = "block" *) logic [WIDTH-1:0] reg_file [NUM_REGS];
+    // Port A
+    reg_addr_t        addr_a;
+    logic             we_a;
+    logic [WIDTH-1:0] wdata_a;
+    logic [WIDTH-1:0] rdata_a;
+    // Port B (using port B for single reads when possible)
+    reg_addr_t        addr_b;
+    logic [WIDTH-1:0] rdata_b;
+
+    // Port A: read src1 / write dst
+    always_ff @(posedge clk) begin
+        if (we_a)
+            reg_file[addr_a] <= wdata_a;
+        rdata_a <= reg_file[addr_a];
+    end
+
+    // Port B: read src2 (read-only)
+    always_ff @(posedge clk) begin
+        rdata_b <= reg_file[addr_b];
+    end
+
+    // -------------------------------------------------------------------------
     // Registers
     // -------------------------------------------------------------------------
 
     // FSM state
     state_e state_q, state_d;
 
-    // Register file
-    logic [WIDTH-1:0] reg_file_q [NUM_REGS];
-    logic [WIDTH-1:0] reg_file_d [NUM_REGS];
+    // Register file handling helper registers
+    typedef enum logic [2:0] {
+        // using encoding such that cnt[idx] shows if the given coordinate is due (0) or done (1)
+        DO_X = 3'b000, // bit 2 = x, if all 3 is due, start with X
+        DO_Y = 3'b100, // bit 1 = y, if X is done, do Y next
+        DO_Z = 3'b110, // bit 0 = z, do Z last
+        DONE = 3'b111
+    } xyz_cnt_t;
+
+    logic     rdata_phase_q, rdata_phase_d;
+    // Lint: two functions (reg_write, reg_copy) in the same always_comb drive these
+    /* verilator lint_off MULTIDRIVEN */
+    xyz_cnt_t xyz_cnt_q    , xyz_cnt_d;
+    /* verilator lint_on MULTIDRIVEN */
 
     // Other registers
     pc_t                  pc_q,       pc_d;
@@ -223,24 +264,65 @@ module ecdsa
     // Register file access helpers
     // -------------------------------------------------------------------------
 
-    function automatic logic [WIDTH-1:0] reg_read(input all_addr_t addr);
+    // Address A1 and B3 has static parameters, add a mux to use those intead of RAM rdata when selected
+    logic [WIDTH-1:0] op_a;
+    logic [WIDTH-1:0] op_b;
+
+    assign op_a = current_instr.src1 == A1 ? CURVE_A1 :
+                  current_instr.src1 == B3 ? CURVE_B3 :
+                                             rdata_a;
+
+    assign op_b = current_instr.src2 == A1 ? CURVE_A1 :
+                  current_instr.src2 == B3 ? CURVE_B3 :
+                                             rdata_b;
+
+    function automatic void reg_write(input all_addr_t addr, input logic [WIDTH-1:0] val);
+        addr_a  = reg_addr_t'(addr);
+        we_a    = 1'b1;
+        wdata_a = val;
+    endfunction
+
+    function automatic xyz_cnt_t get_xyz_condition(input all_addr_t addr);
+        // based on the address, which counter value the register operation should activate on
         case (addr)
-            // A1 and B3 are constants, not part of the actual register file
-            A1      : return CURVE_A1;
-            B3      : return CURVE_B3;
-            // casting might be needed if the actual register file array requires less bit(s) for indexing
-            default : return reg_file_q[reg_addr_t'(addr)];
+            // reused for T2/1/0 as well (using xyz = 210 so x/idx[2] corresponts to T2)
+            X1, X2, X3, T2: return DO_X;
+            Y1, Y2, Y3, T1: return DO_Y;
+            Z1, Z2, Z3, T0: return DO_Z;
+            default:        return DONE;
         endcase
     endfunction
 
-    function automatic void reg_write(input all_addr_t addr, input logic [WIDTH-1:0] val);
-        // Making it explicit to lint that discarding MSB is fine when the widths differ
-        // (The addresses of the constants should not be used for reg_write)
-        if ( addr[$size(all_addr_t)-1] ||
-            !addr[$size(all_addr_t)-1]) begin
+    function automatic void xyz_cnt_next(logic increment);
+        xyz_cnt_d = increment ? xyz_cnt_q.next() : xyz_cnt_q;
+    endfunction
 
-            // casting might be needed if the actual register file array requires less bit(s) for indexing
-            reg_file_d[reg_addr_t'(addr)] = val;
+    function automatic void xyz_reg_write(input all_addr_t addr, input logic [WIDTH-1:0] val);
+        xyz_cnt_t xyz_condition = get_xyz_condition(addr);
+
+        // activate the write and increment the counter on the active value and it's not "all done"
+        if (xyz_cnt_q == xyz_condition && xyz_cnt_q != DONE) begin
+            reg_write(addr, val);
+            xyz_cnt_next(.increment(1'b1));
+        end
+    endfunction
+
+    function automatic void xyz_reg_copy(input all_addr_t to, input all_addr_t from);
+        xyz_cnt_t xyz_condition = get_xyz_condition(to);
+
+        // activate the copy when at the active value and it's not "all done"
+        if (xyz_cnt_q == xyz_condition && xyz_cnt_q != DONE) begin
+            // Note: Using Port A to write, Port B to read, but not overlapping them
+            //       Write happens in the read's data phase
+            if (!rdata_phase_q) begin
+                addr_b        = reg_addr_t'(from);
+                rdata_phase_d = 1'b1;
+                // ensure that counter value is maintained this cycle (it would reset to 0 by default!)
+                xyz_cnt_next(.increment(1'b0));
+            end else begin
+                reg_write(to, rdata_b);
+                xyz_cnt_next(.increment(1'b1));
+            end
         end
     endfunction
 
@@ -260,8 +342,8 @@ module ecdsa
         .rst_n     (rst_n),
         .valid     (arith_valid_q),
         .op        (current_instr.op),
-        .a         (reg_read(current_instr.src1)),
-        .b         (reg_read(current_instr.src2)),
+        .a         (op_a),
+        .b         (op_b),
         .modulus   (modulus),
         .ready     (arith_ready),
         .result    (arith_result)
@@ -322,19 +404,38 @@ module ecdsa
         bit_pos_d      = bit_pos_q;
         arith_valid_d  = arith_valid_q;
 
-        foreach (reg_file_d[i]) begin
-            reg_file_d[i] = reg_file_q[i];
-        end
+        // Register file addresses, write enable and write data
+        // Do instruction operand reads by default
+        addr_a  = reg_addr_t'(current_instr.src1);
+        addr_b  = reg_addr_t'(current_instr.src2);
+        we_a    = 1'b0;
+        wdata_a = '0;
+
+        // Register file helpers
+        rdata_phase_d = 1'b0;   // address phase by default
+        xyz_cnt_d     = DO_X; // default state
 
         // Handle running the program here centrally for all states
+        // Starting the program is the responsibility of the individual FSM states,
+        // mid-program handling and stopping happens here
+
+        // Store result when arith ready
         if (arith_ready) begin
+            addr_a  = reg_addr_t'(current_instr.dst);
+            we_a    = 1'b1;
+            wdata_a = arith_result;
 
-            // When arith block ready, store result
-            reg_write(current_instr.dst, arith_result);
+            // Stop and wait for the next instruction to be loaded
+            arith_valid_d = 1'b0;
+        end
 
-            // If end of program reached, stop the program
-            if (int'(pc_q) inside {PROGRAM_ENDS}) begin
-                arith_valid_d = 1'b0;
+        // Load and auto-start next instruction when not at a program boundary
+        if ( !(int'(pc_q) inside {PROGRAM_STARTS}) ) begin // assuming programs are tightly packed in the ROM
+            if (!arith_valid_q) begin
+                if(rdata_phase_q == 1'b0) begin
+                    rdata_phase_d = 1'b1;
+                    arith_valid_d = 1'b1; // arith_valid should be asserted together with data phase
+                end
             end
         end
 
@@ -343,13 +444,16 @@ module ecdsa
             // -----------------------------------------------------------------
             StIdle: begin
                 if (valid) begin
-                    // Initialize P1 accumulator to point infinity
-                    reg_write(X1, INF_X);
-                    reg_write(Y1, INF_Y);
-                    reg_write(Z1, INF_Z);
+                    // Initialize P1 accumulator to point infinity then move to next state
+                    // Note: counter based write coordinate selection and counter increment are hidden in xyz_reg_write
+                    xyz_reg_write(X1, INF_X);
+                    xyz_reg_write(Y1, INF_Y);
+                    xyz_reg_write(Z1, INF_Z);
 
-                    // Move to next state
-                    state_d = StPrepare;
+                    // Move to next state when all of x, y, and z are initialized
+                    if (xyz_cnt_q == DONE) begin
+                        state_d = StPrepare;
+                    end
                 end
             end
 
@@ -361,22 +465,39 @@ module ecdsa
                 // If program not started yet, load the inputs and start the program
                 if (!arith_valid_q && int'(pc_q) == PREPARE_START) begin
 
-                    reg_write(T0, s);
-                    reg_write(T1, z);
-                    reg_write(T2, r);
+                    // Note: counter based write coordinate selection and counter increment are hidden in xyz_reg_write
+                    // Reusing xyz_reg_write for T2/1/0 a well
+                    xyz_reg_write(T0, s);
+                    xyz_reg_write(T1, z);
+                    xyz_reg_write(T2, r);
 
-                    arith_valid_d = 1'b1;
+                    // Start only when all of x, y, and z are initialized
+                    if (xyz_cnt_q == DONE) begin
+                        arith_valid_d = 1'b1;
+                    end
                 end
 
                 // Nothing to do here when the program running, it's handled outside the case statement
 
                 // When program finished, store the u1, u2 results, initialize loop counter and move to next state
-                if (!arith_valid_q && int'(pc_q) != PREPARE_START) begin
-                    u1_d      = reg_read(T1);
-                    u2_d      = reg_read(T2);
-                    bit_pos_d = BITCNT_W'(WIDTH-1);
+                if (!arith_valid_q && int'(pc_q) == PREPARE_END+1) begin
+                    // Split into address and data phase
+                    if (!rdata_phase_q) begin
+                        // Address phase
 
-                    state_d = StAdd;
+                        addr_a = reg_addr_t'(T1);
+                        addr_b = reg_addr_t'(T2);
+                        rdata_phase_d = 1'b1;
+                    end else begin
+                        // Data phase
+
+                        u1_d      = rdata_a;
+                        u2_d      = rdata_b;
+
+                        bit_pos_d = BITCNT_W'(WIDTH-1);
+
+                        state_d = StAdd;
+                    end
                 end
             end
 
@@ -388,17 +509,21 @@ module ecdsa
                 // If program not started yet, load the inputs and start the program
                 if (!arith_valid_q && int'(pc_q) == POINT_ADD_START) begin
                     // P2 = selected_point (for P1 += P2)
-                    reg_write(X2, sel_x);
-                    reg_write(Y2, sel_y);
-                    reg_write(Z2, sel_z);
+                    // Note: counter based write coordinate selection and counter increment are hidden in xyz_reg_write
+                    xyz_reg_write(X2, sel_x);
+                    xyz_reg_write(Y2, sel_y);
+                    xyz_reg_write(Z2, sel_z);
 
-                    arith_valid_d = 1'b1;
+                    // Start only when all of x, y, and z are initialized
+                    if (xyz_cnt_q == DONE) begin
+                        arith_valid_d = 1'b1;
+                    end
                 end
 
                 // Nothing to do here when the program running, it's handled outside the case statement
 
                 // When program finished, move to next state (results already in the P1 accumulator)
-                if (!arith_valid_q && int'(pc_q) != POINT_ADD_START) begin
+                if (!arith_valid_q && int'(pc_q) == POINT_ADD_END+1) begin
                     // Stop condition: last bit (doubling not needed then)
                     state_d = (bit_pos_q != '0) ? StDouble : StFinalize;
                 end
@@ -412,17 +537,21 @@ module ecdsa
                 // If program not started yet, load the inputs and start the program
                 if (!arith_valid_q && int'(pc_q) == POINT_ADD_START) begin
                     // P2 = P1 (for P1 + P2 = 2*P1)
-                    reg_write(X2, reg_read(X1));
-                    reg_write(Y2, reg_read(Y1));
-                    reg_write(Z2, reg_read(Z1));
+                    // Note: counter based coordinate selection and counter increment are hidden in xyz_reg_copy
+                    xyz_reg_copy(X2, X1);
+                    xyz_reg_copy(Y2, Y1);
+                    xyz_reg_copy(Z2, Z1);
 
-                    arith_valid_d = 1'b1;
+                    // Start only when all of x, y, and z are loaded
+                    if (xyz_cnt_q == 3'b111) begin
+                        arith_valid_d = 1'b1;
+                    end
                 end
 
                 // Nothing to do here when the program running, it's handled outside the case statement
 
                 // When program finished, move back to add state and decrement bit counter (results already in the P1 accumulator)
-                if (!arith_valid_q && int'(pc_q) != POINT_ADD_START) begin
+                if (!arith_valid_q && int'(pc_q) == POINT_ADD_END+1) begin
                     bit_pos_d = bit_pos_q - 1;
                     // Note: could do the same skip StAdd optimization as in mod_mul but
                     // PC loading does not currently support re-running the same state (StDouble after StDouble)
@@ -438,19 +567,32 @@ module ecdsa
                 // If program not started yet, load the inputs and start the program
                 if (!arith_valid_q && int'(pc_q) == FINALIZE_START) begin
                     // X1, Y1, Z1 are already in the corresponding registers
-                    reg_write(T2, r);
+                    xyz_reg_write(T2, r); // Reusing xyz_reg_write for T2/1/0 a well
 
-                    arith_valid_d = 1'b1;
+                    // Start only when T2 is loaded
+                    if (xyz_cnt_q[2]) begin
+                        arith_valid_d = 1'b1;
+                    end
                 end
 
                 // Nothing to do here when the program running, it's handled outside the case statement
 
                 // When program finished, check the result and move back to idle
-                if (!arith_valid_q && int'(pc_q) != FINALIZE_START) begin
-                    ready        = 1'b1;
-                    verif_passed = (reg_read(T0) == '0);
+                if (!arith_valid_q && int'(pc_q) == FINALIZE_END+1) begin
+                    // Read is split into address and data phase
+                    if (!rdata_phase_q) begin
+                        // Address phase
 
-                    state_d = StIdle;
+                        addr_b = reg_addr_t'(T0);
+                        rdata_phase_d = 1'b1;
+                    end else begin
+                        // Data phase
+
+                        ready        = 1'b1;
+                        verif_passed = (rdata_b == '0);
+
+                        state_d = StIdle;
+                    end
                 end
             end
 
@@ -461,19 +603,6 @@ module ecdsa
     // -------------------------------------------------------------------------
     // Sequential: register updates, asynchronous active-low reset
     // -------------------------------------------------------------------------
-
-    // Register file registers
-    always_ff @(posedge clk or negedge rst_n) begin
-        if (!rst_n) begin
-            for (int i = 0; i < NUM_REGS; i++) begin
-                reg_file_q[i] <= '0;
-            end
-        end else begin
-            for (int i = 0; i < NUM_REGS; i++) begin
-                reg_file_q[i] <= reg_file_d[i];
-            end
-        end
-    end
 
     // FSM state register
     always_ff @(posedge clk or negedge rst_n) begin
@@ -505,5 +634,17 @@ module ecdsa
             bit_pos_q      <= bit_pos_d;
         end
     end
+
+    // register file helpers
+    always_ff @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            rdata_phase_q <= 1'b0;
+            xyz_cnt_q     <= DO_X;
+        end else begin
+            rdata_phase_q <= rdata_phase_d;
+            xyz_cnt_q     <= xyz_cnt_d;
+        end
+    end
+
 
 endmodule
